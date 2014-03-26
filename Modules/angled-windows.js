cb6ca0cb69b30b686e007d9128fc45f87526bab3
Michael Conroy <michael.e.conroy@gmail.com>
2014-03-26 16:25:45 -0400

    Added bi-directional scope var "status"

--- Modules/angled-windows.js
+++ Modules/angled-windows.js
@@ -29,7 +29,8 @@ angular.module('angled-windows-directives',['ngSanitize','ngAnimate','angled-dra
 			templateUrl : '/tmpls/window',
 			scope : {
 				id : '@id',
-				title : '@title'
+				title : '@title',
+				status : '=status' // status bar messages bi-directional
 			},
 			link : function(scope,el,attrs){
 				scope.rolledUp = false; // is the content portion of the window hidden
@@ -55,7 +56,7 @@ angular.module('angled-windows-directives',['ngSanitize','ngAnimate','angled-dra
 	}]) // end angledWindow
 	
 	.run(['$templateCache',function($templateCache){
-		$templateCache.put('/tmpls/window','<div class="angled-window" angled-draggable="{handle: \'div.panel-heading\',opacity: 0.5}"> <div class="panel panel-primary" angled-resizable="{handles: \'se\', alsoResize: \'#windowBodyContent\'}"> <div class="panel-heading cursor-move" id="windowHeading"> <span class="pull-right"> <a type="button" id="windowMinimizeBtn" class="btn btn-primary btn-xs" ng-click="rollUp()"> <span class="glyphicon" ng-class="{false: \'glyphicon-chevron-up\',true: \'glyphicon-chevron-down\'}[rolledUp]"></span> </a> </span> <span class="angled-window-title" ng-bind-html="title"></span> </div> <div id="windowBody" class="angled-window-body"> <div class="panel-body angled-window-body-content" id="windowBodyContent" ng-transclude></div> <div class="panel-footer"> <span class="text-muted angled-window-status-bar"><small>&nbsp;{{statusMsg}}</small></span> <span class="pull-right glyphicon glyphicon-resize-full ui-resizable-handle ui-resizable-se"></span> </div> </div> </div> </div>');
+		$templateCache.put('/tmpls/window','<div class="angled-window" angled-draggable="{handle: \'div.panel-heading\',opacity: 0.5}"> <div class="panel panel-primary" angled-resizable="{handles: \'se\', alsoResize: \'#windowBodyContent\'}"> <div class="panel-heading cursor-move" id="windowHeading"> <span class="pull-right"> <a type="button" id="windowMinimizeBtn" class="btn btn-primary btn-xs" ng-click="rollUp()"> <span class="glyphicon" ng-class="{false: \'glyphicon-chevron-up\',true: \'glyphicon-chevron-down\'}[rolledUp]"></span> </a> </span> <span class="angled-window-title" ng-bind-html="title"></span> </div> <div id="windowBody" class="angled-window-body"> <div class="panel-body angled-window-body-content" id="windowBodyContent" ng-transclude></div> <div class="panel-footer"> <span class="text-muted angled-window-status-bar"><small>&nbsp;{{status}}</small></span> <span class="pull-right glyphicon glyphicon-resize-full ui-resizable-handle ui-resizable-se"></span> </div> </div> </div> </div>');
 	}]); // end run
 	
 // declare main module
