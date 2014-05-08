7e6de9d5c453a2fbb4bbdd88c86bda54609b303c
Michael Conroy <michael.e.conroy@gmail.com>
2014-05-08 16:25:24 -0400

    Added comments

--- Services/angled-storage.js
+++ Services/angled-storage.js
@@ -13,10 +13,20 @@
  	.service('angledGenericStorageSrv',['$log',function($log){
  		var storage = undefined;
 
+ 		/**
+ 		 * Log Error
+ 		 * Logs errors to the browser console.
+ 		 * @param	e 		Exception Object
+ 		 */
  		var _logError = function(e){
 			$log.error(e.name + ' (' + e.fileName + ':' + e.lineNumber + ') : ' + e.message);
  		}; // end logError
 
+ 		/**
+ 		 * Set Storage
+ 		 * Set the service's storage object.
+ 		 * @param	s 		object
+ 		 */
  		this.setStorage : function(s){
  			try{
  				if(angular.isUndefined(s) || !angular.isObject(s))
@@ -27,9 +37,14 @@
  			return false;
  		}; // end setStorage
 
+ 		/**
+ 		 * Get
+ 		 * Retrieve the value of the given key.
+ 		 * @param	key 	string
+ 		 */
  		this.get : function(key){
  			try{
- 				if(angular.isUndefined(key) || angular.equals(key,'') || angular.equals(key,null))
+ 				if(angular.isUndefined(key) || !angular.isString(key))
  					throw "Key is undefined.";
  				
  				return angular.fromJson(storage.getItem(key));
@@ -37,6 +52,12 @@
  			return undefined;
  		}; // end get
 
+ 		/**
+ 		 * Save
+ 		 * Save the given data with the index of the value key.
+ 		 * @param	key 	string
+ 		 * @param	data 	mixed
+ 		 */
  		this.save : function(key,data){
  			try{
  				if(angular.isUndefined(key) || !angular.isString(key))
@@ -50,6 +71,11 @@
  			return false;
  		}; // end save
 
+ 		/**
+ 		 * Delete
+ 		 * Delete the value of the given key from storage.
+ 		 * @param	key 	string
+ 		 */
  		this.del : function(key){
  			try{
  				if(angular.isUndefined(key) || !angular.isString(key))
@@ -61,6 +87,11 @@
  			return false;
  		}; // end del
 
+ 		/**
+ 		 * Key
+ 		 * Retrieve the key given by the index i.
+ 		 * @param	i 		int
+ 		 */
  		this.key : function(i){
  			try{
  				if(angular.isUndefined(i) || !angular.isNumber(i))
@@ -72,6 +103,10 @@
  			return undefined;
  		}; // end key
 
+ 		/**
+ 		 * Clear
+ 		 * Clean out the storage by deleting all key/value pairs.
+ 		 */
  		this.clear : function(){
  			try{
  				storage.clear();
@@ -80,6 +115,10 @@
  			return false;
  		}; // end clear
 
+ 		/**
+ 		 * Length
+ 		 * Returns the number of keys in storage.
+ 		 */
  		this.length : function(){
  			try{
  				return storage.length;
@@ -96,7 +135,8 @@
  			/**
  			 * Get Storage Type
  			 *
- 			 * Returns a service based on the requested storage type, default is sessionStorage.
+ 			 * Returns a service based on the requested storage type, default is sessionStorage,
+ 			 * undefined on error.
  			 *
  			 * @param	type 	string
  			 */
@@ -126,6 +166,11 @@
  				return undefined;
  			}, // end get
 
+ 			/**
+ 			 * Get Local
+ 			 *
+ 			 * Returns a service who's storage is the localStorage, or undefined
+ 			 */
  			getLocal : function(){
  				if(local)
  					if(storageSrv.setStorage($window.localStorage))
@@ -136,6 +181,11 @@
  				return undefined;
  			}, // end getLocal
 
+ 			/**
+ 			 * Get Session
+ 			 *
+ 			 * Returns a service who's storage is the sessionStorage, or undefined.
+ 			 */
  			getSession : function(){
  				if(session)
  					if(storageSrv.setStorage($window.sessionStorage))
